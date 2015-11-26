8a5cc3cf096391780bee436d81031c7e1356f69c
Samantha Morco <morkotiks@gmail.com>
2015-11-25 17:49:33 -0800

    fixing boolean

--- db/migrate/20151118023419_change_stock_to_boolean.rb
+++ db/migrate/20151118023419_change_stock_to_boolean.rb
@@ -1,5 +1,5 @@
 class ChangeStockToBoolean < ActiveRecord::Migration
   def change
-    change_column :products, :stock_info, 'boolean USING (trim(stock_info)::boolean', default: true
+    change_column :products, :stock_info, 'boolean USING (trim(stock_info)::boolean)', default: true
   end
 end
