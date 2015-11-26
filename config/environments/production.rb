0f1720048847fb10757b8143e443d37933ef51b5
Samantha Morco <morkotiks@gmail.com>
2015-11-25 18:04:52 -0800

    trying to fix background image in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #trying to fix background image
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
